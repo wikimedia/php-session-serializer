b21b5ac9f0a060ad3a54f096f447f8d5a0a0d507
Brad Jorsch <bjorsch@wikimedia.org>
2015-10-14 12:04:51 -0400

    Initial checkin
    Change-Id: I9c083d47bb5c1a9e06223634f816a4ffb89036b9

---
+++ tests/Wikimedia/PhpSessionSerializerTest.php
@@ -0,0 +1,627 @@
+<?php
+/**
+ * php-session-serializer
+ *
+ * Copyright (C) 2015 Brad Jorsch <bjorsch@wikimedia.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @author Brad Jorsch <bjorsch@wikimedia.org>
+ */
+
+namespace Wikimedia;
+
+use Wikimedia\PhpSessionSerializer;
+use Psr\Log\LogLevel;
+
+// Wikimedia\PhpSessionSerializer relies on the auto-importing of ini_set and
+// ini_get from the global namespace. In this unit test, we override these with
+// a namespace-local version to allow for testing failure modes.
+$wgMockIniInstance = null;
+
+function ini_set( $var, $value ) {
+	global $wgMockIniInstance;
+	return $wgMockIniInstance
+		? $wgMockIniInstance->mockIniSet( $var, $value )
+		: \ini_set( $var, $value );
+}
+
+function ini_get( $var ) {
+	global $wgMockIniInstance;
+	return $wgMockIniInstance
+		? $wgMockIniInstance->mockIniGet( $var )
+		: \ini_get( $var );
+}
+
+/**
+ * @covers Wikimedia\PhpSessionSerializer
+ */
+class PhpSessionSerializerTest extends \PHPUnit_Framework_TestCase {
+
+	protected $oldFormat;
+
+	protected $mockIniAllowed;
+	protected $mockIniValue;
+
+	protected static $standardArray, $longKey, $closure;
+
+	public function mockIniGet( $var ) {
+		if ( $var !== 'session.serialize_handler' || !is_array( $this->mockIniAllowed ) ) {
+			return \ini_get( $var );
+		}
+
+		return $this->mockIniValue;
+	}
+
+	public function mockIniSet( $var, $value ) {
+		if ( $var !== 'session.serialize_handler' || !is_array( $this->mockIniAllowed ) ) {
+			return \ini_set( $var, $value );
+		}
+
+		if ( in_array( $value, $this->mockIniAllowed, true ) ) {
+			$old = $this->mockIniValue;
+			$this->mockIniValue = $value;
+			return $old;
+		} else {
+			// trigger_error to test that warnings are properly suppressed
+			trigger_error( "mockIniSet disallows setting to '$value'", E_USER_WARNING );
+			return false;
+		}
+	}
+
+	protected static function initTestData() {
+		self::$longKey = str_pad( 'long key ', 128, '-' );
+		self::$standardArray = array(
+			'true' => true,
+			'false' => false,
+			'int' => 42,
+			'zero' => 0,
+			'double' => 12.34,
+			'inf' => INF,
+			'-inf' => -INF,
+			'string' => 'string',
+			'empty string' => '',
+			'array' => array( 0, 1, 100 => 100, 3 => 3, 2 => 2, 'foo' => 'bar' ),
+			'empty array' => array(),
+			'object' => (object)array( 'foo' => 'foo' ),
+			'empty object' => new \stdClass,
+			'null' => null,
+			'' => 'empty key',
+			42 => 42,
+			self::$longKey => 'long key',
+		);
+
+		self::$closure = function () {
+		};
+	}
+
+	protected function setUp() {
+		global $wgMockIniInstance;
+		parent::setUp();
+		$this->oldFormat = \ini_get( 'session.serialize_handler' );
+		PhpSessionSerializer::setLogger( new TestLogger() );
+		$wgMockIniInstance = $this;
+		$this->mockIniValue = $this->oldFormat;
+	}
+
+	protected function tearDown() {
+		global $wgMockIniInstance;
+		$wgMockIniInstance = null;
+		\ini_set( 'session.serialize_handler', $this->oldFormat );
+		parent::tearDown();
+	}
+
+	public function testSetSerializeHandler() {
+		// Test normal operation
+		$ret = PhpSessionSerializer::setSerializeHandler();
+		$this->assertSame( $ret, \ini_get( 'session.serialize_handler' ) );
+
+		// Test setting php_serialize
+		$this->mockIniAllowed = array( 'php_serialize', 'php', 'php_binary' );
+		$this->mockIniValue = 'php_binary';
+		$ret = PhpSessionSerializer::setSerializeHandler();
+		$this->assertSame( 'php_serialize', $this->mockIniValue );
+		$this->assertSame( $ret, $this->mockIniValue );
+
+		// Test defaulting to current if it's supported
+		$this->mockIniAllowed = array( 'php', 'php_binary' );
+		$this->mockIniValue = 'php_binary';
+		$ret = PhpSessionSerializer::setSerializeHandler();
+		$this->assertSame( 'php_binary', $this->mockIniValue );
+		$this->assertSame( $ret, $this->mockIniValue );
+
+		// Test choosing a supported format
+		$this->mockIniAllowed = array( 'php', 'php_binary' );
+		$this->mockIniValue = 'bogus';
+		$ret = PhpSessionSerializer::setSerializeHandler();
+		$this->assertSame( 'php', $this->mockIniValue );
+		$this->assertSame( $ret, $this->mockIniValue );
+
+		// Test failure
+		$this->mockIniAllowed = array( 'nothing', 'useful' );
+		$this->mockIniValue = 'bogus';
+		try {
+			PhpSessionSerializer::setSerializeHandler();
+			$this->fail( 'Expected exception not thrown' );
+		} catch ( \DomainException $ex ) {
+			$this->assertSame(
+				'Failed to set serialize handler to a supported format.' .
+					' Supported formats are: php_serialize, php, php_binary.',
+				$ex->getMessage()
+			);
+		}
+	}
+
+	public static function provideHandlers() {
+		return array(
+			array( 'php', 'test|b:1;' ),
+			array( 'php_binary', "\x04testb:1;" ),
+			array( 'php_serialize', 'a:1:{s:4:"test";b:1;}' ),
+			array( 'bogus', new \DomainException( 'Unsupported format "bogus"' ) ),
+			array(
+				false,
+				new \UnexpectedValueException( 'Could not fetch the value of session.serialize_handler' )
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider provideHandlers
+	 */
+	public function testEncode( $format, $encoded ) {
+		$this->mockIniAllowed = array( 'php_serialize', 'php', 'php_binary' );
+		$this->mockIniValue = $format;
+
+		$data = array( 'test' => true );
+		if ( $encoded instanceof \Exception ) {
+			try {
+				PhpSessionSerializer::encode( $data );
+				$this->fail( 'Expected exception not thrown' );
+			} catch ( \Exception $ex ) {
+				$this->assertInstanceOf( get_class( $encoded ), $ex );
+				$this->assertSame( $encoded->getMessage(), $ex->getMessage() );
+			}
+		} else {
+			$this->assertSame( $encoded, PhpSessionSerializer::encode( $data ) );
+		}
+	}
+
+	/**
+	 * @dataProvider provideHandlers
+	 */
+	public function testDecode( $format, $encoded ) {
+		$this->mockIniAllowed = array( 'php_serialize', 'php', 'php_binary' );
+		$this->mockIniValue = $format;
+
+		$data = array( 'test' => true );
+		if ( $encoded instanceof \Exception ) {
+			try {
+				PhpSessionSerializer::decode( '' );
+				$this->fail( 'Expected exception not thrown' );
+			} catch ( \Exception $ex ) {
+				$this->assertInstanceOf( get_class( $encoded ), $ex );
+				$this->assertSame( $encoded->getMessage(), $ex->getMessage() );
+			}
+		} else {
+			$this->assertSame( $data, PhpSessionSerializer::decode( $encoded ) );
+		}
+	}
+
+	public static function provideEncodePhp() {
+		self::initTestData();
+		return array(
+			'std' => array(
+				self::$standardArray,
+				'true|b:1;false|b:0;int|i:42;zero|i:0;double|d:12.34;inf|d:INF;-inf|d:-INF;string|s:' .
+					'6:"string";empty string|s:0:"";array|a:6:{i:0;i:0;i:1;i:1;i:100;i:100;i:3;i:3;i:2;i' .
+					':2;s:3:"foo";s:3:"bar";}empty array|a:0:{}object|O:8:"stdClass":1:{s:3:"foo";s:3:"f' .
+					'oo";}empty object|O:8:"stdClass":0:{}null|N;|s:9:"empty key";long key -------------' .
+					'-----------------------------------------------------------------------------------' .
+					'-----------------------|s:8:"long key";',
+				array(
+					array( LogLevel::WARNING, 'Ignoring unsupported integer key "42"' ),
+				),
+			),
+			array(
+				array( 'pipe|key' => 'piped' ),
+				null,
+				array(
+					array( LogLevel::ERROR, 'Serialization failed: Key with unsupported characters "pipe|key"' ),
+				),
+			),
+			array(
+				array( 'bang!key' => 'banged' ),
+				null,
+				array(
+					array( LogLevel::ERROR, 'Serialization failed: Key with unsupported characters "bang!key"' ),
+				),
+			),
+			array(
+				array( 'nan' => NAN ),
+				'nan|d:NAN;',
+				array(),
+			),
+			array(
+				array( 'function' => self::$closure ),
+				null,
+				array(
+					array( LogLevel::ERROR, 'Value serialization failed: [serialize Closure error]' ),
+				),
+			),
+		);
+	}
+
+	public static function provideDecodePhp() {
+		$ret = array_filter( self::provideEncodePhp(), function ( $x ) {
+			return $x[1] !== null;
+		} );
+		unset( $ret['std'][0][42] );
+		$ret['std'][2] = array();
+
+		$ret[] = array(
+			null,
+			'test|i:042;',
+			array(
+				array(
+					LogLevel::ERROR, 'Value unserialization failed: read value does not match original string'
+				)
+			),
+		);
+		$ret[] = array(
+			null,
+			'test|i:42',
+			array(
+				array(
+					LogLevel::ERROR, 'Value unserialization failed: [unserialize error]'
+				)
+			),
+		);
+		$ret[] = array(
+			null,
+			'test|i:42;X|',
+			array(
+				array( LogLevel::ERROR, 'Unserialize failed: unexpected end of string' )
+			),
+		);
+		$ret[] = array(
+			array( 'test' => 42, 'test2' => 43 ),
+			'test|i:42;test2|i:43;X!',
+			array(),
+		);
+		$ret[] = array(
+			array( 'test' => 42, 'X!test2' => 43 ),
+			'test|i:42;X!test2|i:43;',
+			array(
+				array( LogLevel::WARNING, 'Decoding found a key with unsupported characters: "X!test2"' )
+			),
+		);
+		$ret[] = array(
+			array(),
+			'test!',
+			array(),
+		);
+		$ret[] = array(
+			array( 'test' => 42 ),
+			'test|i:42;test2',
+			array(
+				array( LogLevel::WARNING, 'Ignoring garbage at end of string' )
+			),
+		);
+
+		return $ret;
+	}
+
+	/**
+	 * @dataProvider provideEncodePhp
+	 */
+	public function testEncodePhp( $data, $encoded, $log ) {
+		$logger = new TestLogger( true );
+		PhpSessionSerializer::setLogger( $logger );
+		$this->assertSame( $encoded, PhpSessionSerializer::encodePhp( $data ) );
+		$this->assertSame( $log, $logger->getBuffer() );
+	}
+
+	/**
+	 * @dataProvider provideDecodePhp
+	 */
+	public function testDecodePhp( $data, $encoded, $log ) {
+		$logger = new TestLogger( true );
+		PhpSessionSerializer::setLogger( $logger );
+		if ( isset( $data['nan'] ) ) {
+			$ret = PhpSessionSerializer::decodePhp( $encoded );
+			$this->assertTrue( is_nan( $ret['nan'] ) );
+		} else {
+			$this->assertEquals( $data, PhpSessionSerializer::decodePhp( $encoded ) );
+		}
+		$this->assertSame( $log, $logger->getBuffer() );
+	}
+
+	public static function provideEncodePhpBinary() {
+		self::initTestData();
+		return array(
+			'std' => array(
+				self::$standardArray,
+				"\x04trueb:1;\x05falseb:0;\x03inti:42;\x04zeroi:0;\x06doubled:12.34;\x03infd:INF;\x04" .
+					"-infd:-INF;\x06strings:6:\"string\";\x0cempty strings:0:\"\";\x05arraya:6:{i:0;i:0;i:1;" .
+					"i:1;i:100;i:100;i:3;i:3;i:2;i:2;s:3:\"foo\";s:3:\"bar\";}\x0bempty arraya:0:{}\x06object" .
+					"O:8:\"stdClass\":1:{s:3:\"foo\";s:3:\"foo\";}\x0cempty objectO:8:\"stdClass\":0:{}\x04" .
+					"nullN;\0s:9:\"empty key\";",
+				array(
+					array( LogLevel::WARNING, 'Ignoring unsupported integer key "42"' ),
+					array( LogLevel::WARNING, 'Ignoring overlong key "' . self::$longKey . '"' ),
+				),
+			),
+			array(
+				array( 'pipe|key' => 'piped' ),
+				"\x08pipe|keys:5:\"piped\";",
+				array(),
+			),
+			array(
+				array( 'bang!key' => 'banged' ),
+				"\x08bang!keys:6:\"banged\";",
+				array(),
+			),
+			array(
+				array( 'nan' => NAN ),
+				"\x03nand:NAN;",
+				array(),
+			),
+			array(
+				array( 'function' => self::$closure ),
+				null,
+				array(
+					array( LogLevel::ERROR, 'Value serialization failed: [serialize Closure error]' ),
+				),
+			),
+		);
+	}
+
+	public static function provideDecodePhpBinary() {
+		$ret = array_filter( self::provideEncodePhpBinary(), function ( $x ) {
+			return $x[1] !== null;
+		} );
+		unset( $ret['std'][0][42] );
+		unset( $ret['std'][0][self::$longKey] );
+		$ret['std'][2] = array();
+
+		$ret[] = array(
+			null,
+			"\x04testi:042;",
+			array(
+				array(
+					LogLevel::ERROR, 'Value unserialization failed: read value does not match original string'
+				)
+			),
+		);
+		$ret[] = array(
+			null,
+			"\x04testi:42",
+			array(
+				array(
+					LogLevel::ERROR, 'Value unserialization failed: [unserialize error]'
+				)
+			),
+		);
+		$ret[] = array(
+			null,
+			"\x50test",
+			array(
+				array(
+					LogLevel::ERROR, 'Unserialize failed: unexpected end of string'
+				)
+			),
+		);
+		$ret[] = array(
+			null,
+			"\x04test",
+			array(
+				array(
+					LogLevel::ERROR, 'Unserialize failed: unexpected end of string'
+				)
+			),
+		);
+		$ret[] = array(
+			array( 'test' => 42, 'test2' => 43 ),
+			"\x04testi:42;\x05test2i:43;\x81X",
+			array(),
+		);
+		$ret[] = array(
+			array(),
+			"\x84test",
+			array(),
+		);
+
+		return $ret;
+	}
+
+	/**
+	 * @dataProvider provideEncodePhpBinary
+	 */
+	public function testEncodePhpBinary( $data, $encoded, $log ) {
+		$logger = new TestLogger( true );
+		PhpSessionSerializer::setLogger( $logger );
+		$this->assertSame( $encoded, PhpSessionSerializer::encodePhpBinary( $data ) );
+		$this->assertSame( $log, $logger->getBuffer() );
+	}
+
+	/**
+	 * @dataProvider provideDecodePhpBinary
+	 */
+	public function testDecodePhpBinary( $data, $encoded, $log ) {
+		$logger = new TestLogger( true );
+		PhpSessionSerializer::setLogger( $logger );
+		if ( isset( $data['nan'] ) ) {
+			$ret = PhpSessionSerializer::decodePhpBinary( $encoded );
+			$this->assertTrue( is_nan( $ret['nan'] ) );
+		} else {
+			$this->assertEquals( $data, PhpSessionSerializer::decodePhpBinary( $encoded ) );
+		}
+		$this->assertSame( $log, $logger->getBuffer() );
+	}
+
+	public static function provideEncodePhpSerialize() {
+		self::initTestData();
+		return array(
+			array(
+				self::$standardArray,
+				'a:17:{s:4:"true";b:1;s:5:"false";b:0;s:3:"int";i:42;s:4:"zero";i:0;s:6:"double";d:1' .
+					'2.34;s:3:"inf";d:INF;s:4:"-inf";d:-INF;s:6:"string";s:6:"string";s:12:"empty string"' .
+					';s:0:"";s:5:"array";a:6:{i:0;i:0;i:1;i:1;i:100;i:100;i:3;i:3;i:2;i:2;s:3:"foo";s:3:"' .
+					'bar";}s:11:"empty array";a:0:{}s:6:"object";O:8:"stdClass":1:{s:3:"foo";s:3:"foo";}s' .
+					':12:"empty object";O:8:"stdClass":0:{}s:4:"null";N;s:0:"";s:9:"empty key";i:42;i:42;' .
+					's:128:"long key --------------------------------------------------------------------' .
+					'---------------------------------------------------";s:8:"long key";}',
+				array(),
+			),
+			array(
+				array( 'pipe|key' => 'piped' ),
+				'a:1:{s:8:"pipe|key";s:5:"piped";}',
+				array(),
+			),
+			array(
+				array( 'bang!key' => 'banged' ),
+				'a:1:{s:8:"bang!key";s:6:"banged";}',
+				array(),
+			),
+			array(
+				array( 'nan' => NAN ),
+				'a:1:{s:3:"nan";d:NAN;}',
+				array(),
+			),
+			array(
+				array( 'function' => self::$closure ),
+				null,
+				array(
+					array( LogLevel::ERROR, 'PHP serialization failed: [serialize Closure error]' ),
+				),
+			),
+		);
+	}
+
+	public static function provideDecodePhpSerialize() {
+		$ret = array_filter( self::provideEncodePhpSerialize(), function ( $x ) {
+			return $x[1] !== null;
+		} );
+
+		$ret[] = array(
+			null,
+			'Bogus',
+			array(
+				array(
+					LogLevel::ERROR, 'PHP unserialization failed: [unserialize error]'
+				)
+			),
+		);
+		$ret[] = array(
+			null,
+			'O:8:"stdClass":1:{s:3:"foo";s:3:"bar";}',
+			array(
+				array( LogLevel::ERROR, 'PHP unserialization failed (value was not an array)' )
+			),
+		);
+
+		return $ret;
+	}
+
+	/**
+	 * @dataProvider provideEncodePhpSerialize
+	 */
+	public function testEncodePhpSerialize( $data, $encoded, $log ) {
+		$logger = new TestLogger( true );
+		PhpSessionSerializer::setLogger( $logger );
+		$this->assertSame( $encoded, PhpSessionSerializer::encodePhpSerialize( $data ) );
+		$this->assertSame( $log, $logger->getBuffer() );
+	}
+
+	/**
+	 * @dataProvider provideDecodePhpSerialize
+	 */
+	public function testDecodePhpSerialize( $data, $encoded, $log ) {
+		$logger = new TestLogger( true );
+		PhpSessionSerializer::setLogger( $logger );
+		if ( isset( $data['nan'] ) ) {
+			$ret = PhpSessionSerializer::decodePhpSerialize( $encoded );
+			$this->assertTrue( is_nan( $ret['nan'] ) );
+		} else {
+			$this->assertEquals( $data, PhpSessionSerializer::decodePhpSerialize( $encoded ) );
+		}
+		$this->assertSame( $log, $logger->getBuffer() );
+	}
+
+	public static function provideDecoders() {
+		return array(
+			array( 'decode' ),
+			array( 'decodePhp' ),
+			array( 'decodePhpBinary' ),
+			array( 'decodePhpSerialize' ),
+		);
+	}
+
+	/**
+	 * @dataProvider provideDecoders
+	 * @expectedException InvalidArgumentException
+	 * @expectedExceptionMessage $data must be a string
+	 */
+	public function testDecoderTypeCheck( $method ) {
+		call_user_func( array( '\\Wikimedia\\PhpSessionSerializer', $method ), 1 );
+	}
+
+}
+
+class TestLogger extends \Psr\Log\AbstractLogger {
+	private $collect = false;
+	private $redact = null;
+	private $buffer = array();
+
+	public function __construct( $collect = false, $closure = null ) {
+		$this->collect = $collect;
+		$this->redact = null;
+	}
+
+	public function getBuffer() {
+		return $this->buffer;
+	}
+
+	public function log( $level, $message, array $context = array() ) {
+		$message = trim( $message );
+
+		if ( $this->collect ) {
+			// HHVM and Zend produce different error messages, correct for that.
+			$message = preg_replace(
+				'/(?:' . join( '|', array(
+					'unserialize\(\): Error at offset 0 of [45] bytes',
+					'Unable to unserialize: \[.*\]. Unexpected end of buffer during unserialization.',
+					'Unable to unserialize: \[Bogus\]. Expected \':\' but got \'o\'.',
+				) ) . ')$/',
+				'[unserialize error]',
+				$message
+			);
+			$message = preg_replace(
+				'/(?:' . join( '|', array(
+					'Serialization of \'Closure\' is not allowed',
+					'Attempted to serialize unserializable builtin class Closure\$\S+'
+				) ) . ')$/',
+				'[serialize Closure error]',
+				$message
+			);
+
+			$this->buffer[] = array( $level, $message );
+		} else {
+			echo "LOG[$level]: $message\n";
+		}
+	}
+}
