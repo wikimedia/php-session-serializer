114454b468d95836022a465b4ef9ebff4b05099b
Reedy <reedy@wikimedia.org>
2022-03-15 02:17:09 +0000

    Add phan
    Bug: T303786
    Change-Id: If77d7c951790066029e6dff13fb65c6c143203eb

---
+++ .phan/config.php
@@ -0,0 +1,9 @@
+<?php
+
+$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+
+$cfg['directory_list'][] = 'vendor/';
+
+$cfg['exclude_analysis_directory_list'][] = 'vendor/';
+
+return $cfg;
